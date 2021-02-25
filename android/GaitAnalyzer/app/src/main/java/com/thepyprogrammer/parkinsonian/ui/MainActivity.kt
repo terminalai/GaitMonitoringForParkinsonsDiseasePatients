f37cdfa42d837f7ac1434c14c3a6708bd22fbbdb
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-25 23:46:29 +0800

    Move MainActivity to ui subpackage

--- android/GaitAnalyzer/app/src/main/java/com/thepyprogrammer/parkinsonian/ui/MainActivity.kt
+++ android/GaitAnalyzer/app/src/main/java/com/thepyprogrammer/parkinsonian/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.thepyprogrammer.parkinsonian
+package com.thepyprogrammer.parkinsonian.ui
 
 import android.os.Bundle
 import android.view.Menu
@@ -13,6 +13,7 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.thepyprogrammer.parkinsonian.R
 
 class MainActivity : AppCompatActivity() {
 
