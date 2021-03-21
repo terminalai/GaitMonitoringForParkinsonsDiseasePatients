83d0a9661d8b669b4df2bf73737345e44a30a21b
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-21 23:02:20 +0800

    Update FFTUnitTest.kts

--- android/app/src/test/java/com/thepyprogrammer/gaitanalyzeralgos/FFTUnitTest.kts
+++ android/app/src/test/java/com/thepyprogrammer/gaitanalyzeralgos/FFTUnitTest.kts
@@ -1,5 +1,7 @@
 package com.thepyprogrammer.gaitanalyzeralgos
 
+import com.thepyprogrammer.gaitanalyzer.model.signalProcessing.Complex
+import com.thepyprogrammer.gaitanalyzer.model.signalProcessing.FFT
 import kotlin.math.sqrt
 
 val arr = arrayOf(Complex(0.0), Complex(0.5), Complex(sqrt(3.0)/2), Complex(1.0), Complex(sqrt(3.0)/2), Complex(0.5), Complex(.0), Complex(-0.5)) // y = sin(pi*t/3)
