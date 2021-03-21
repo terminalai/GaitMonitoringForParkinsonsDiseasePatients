c7fbe0e82297ef56b76476899790ac4783f1339b
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-21 23:01:46 +0800

    Update FFT.kt

--- android/app/src/main/java/com/thepyprogrammer/gaitanalyzer/model/signalProcessing/FFT.kt
+++ android/app/src/main/java/com/thepyprogrammer/gaitanalyzer/model/signalProcessing/FFT.kt
@@ -1,4 +1,4 @@
-package com.thepyprogrammer.gaitanalyzeralgos
+package com.thepyprogrammer.gaitanalyzer.model.signalProcessing
 
 object FFT {
     fun fft(a: Array<Complex>) = _fft(a, Complex(0.0, 2.0), 1.0)
