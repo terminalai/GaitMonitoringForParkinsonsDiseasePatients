36dee5d9093f61ca4986c10663a6de2fdee6b870
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-04-08 22:33:57 +0800

    Update StringUtils.kt

--- android/app/src/main/java/com/thepyprogrammer/gaitanalyzer/model/utils/string/StringUtils.kt
+++ android/app/src/main/java/com/thepyprogrammer/gaitanalyzer/model/utils/string/StringUtils.kt
@@ -14,3 +14,33 @@ fun String.isAlpha(): Boolean {
 }
 
 fun String.isPalindrome() = (length <= 1) || equals(StringBuilder(this).reverse().toString())
+
+fun String.toTitleCase(): String {
+    if (isNullOrBlank()) {
+        return this
+    }
+
+    return split(" ").map { word ->
+        word.foldIndexed("") { index, working, char ->
+            val nextChar = if (index == 0) char.toUpperCase() else char.toLowerCase()
+            "$working$nextChar"
+        }
+    }.reduceIndexed { index, working, word ->
+        if (index > 0) "$working $word" else word
+    }
+}
+
+fun String.nameToLogin(): String {
+    if (isNullOrBlank()) {
+        return this
+    }
+    var working = ""
+    toCharArray().forEach { char ->
+        if (char.isLetterOrDigit()) {
+            working += char.toLowerCase()
+        } else if (char.isWhitespace() and !working.endsWith(".")) {
+            working += "."
+        }
+    }
+    return working
+}
