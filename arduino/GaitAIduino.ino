5d66c4272d3284c09ca6b5645ec6a29f26660416
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-06-07 20:17:16 +0800

    Update GaitAIduino

--- arduino/GaitAIduino.ino
+++ arduino/GaitAIduino.ino
@@ -1,19 +1,3 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the Uno and
-  Leonardo, it is attached to digital pin 13. If you're unsure what
-  pin the on-board LED is connected to on your Arduino model, check
-  the documentation at http://arduino.cc
-
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
- */
-
-
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 13 as an output.
